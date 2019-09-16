051be5abd4e3eb7d04ba1e6dd44f5015f5173400
nyamiki <namiki66@outlook.jp>
2019-09-16 16:00:13 +0900

    first commit

---
+++ chalenge3.py
@@ -0,0 +1,38 @@
+# 1
+print("tyappi")
+print("hulk")
+print("Hina Tanaka")
+
+# 2
+x = 10
+if x <10 :
+    print("xは１０未満です")
+else :
+    print("xは１０以上です")
+
+# 3
+y = 100
+if y <= 10 :
+    print("クソだね")
+elif y > 10 and y <= 25:
+    print("まあまあだ")
+else:
+    print("グレイト")
+
+# 4
+print(31 % 2)
+
+# 5
+print(31 // 2)
+
+# 6
+age = 22
+if age < 18:
+    print("You're child")
+elif age >= 18 and age < 65:
+    print("You're adult")
+else:
+    print("You're elder")
+
+
+# ALL CLEAR
